b52afa94feebb18bbd78051a1e1324cdfc0c590e
Roman Sokalski <62810569+romsok24@users.noreply.github.com>
2020-03-29 15:38:11 +0200

    Add files via upload

---
+++ net_stats.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#IFACES="$(cat /dane/net_ifaces | ssh -T admin@10.200.10.18 )"
+RXTX="$(cat /dane/net_rxtx | ssh -T admin@10.200.10.18 )"
+
+printf  "Statystyki uzycia sieci na ASUS\n"
+#echo "interface RX[GB]  TX[GB]" | column -t
+#paste <(printf %s "$IFACES") <(printf %s "$RXTX") | grep -v "0 \[B"  | column -t #| awk '{if ($5=="MiB") print $4/1024 else print $4}' | column -t
+echo -e "interface RX[GB] TX[GB] SUM[GB]\n$RXTX" | column -t
+
+
+echo
+#echo $( ssh admin@10.200.10.18 uptime)
