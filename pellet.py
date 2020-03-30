1cff90f18b84dd54c9e88740847e4fd94f6f7c67
Roman Sokalski <62810569+romsok24@users.noreply.github.com>
2020-03-30 19:17:06 +0200

    Update pellet.py

--- pellet.py
+++ pellet.py
@@ -13,9 +13,9 @@ while True:
   dateczka=datetime.now().strftime("%d %b, %H:%M")
   if(GPIO.input(21) == 0):
       print('Uwaga - sprawdz poziom pelletu w zasobniku! Stan na dzien {dateczka}'.format(dateczka=dateczka))
-      os.system("/bin/echo -e 'Sprawdz poziom pelletu w zasobniku. \nZasypu wystarczy tylko na ok 1.5 dnia palenia, liczac od pierwszego komunikatu! \n\n\nAlert zostal wygenerowany automatycznie - nie odpowiadaj na niego.' | /usr/bin/mailx -s 'Sprawdz poziom pelletu' o2o2@gazeta.pl -a 'From: Kotlownia Sliwice'")
-      os.system("/bin/echo -e 'Sprawdz poziom pelletu w zasobniku. \nZasypu wystarczy tylko na ok 1.5 dnia palenia, liczac od pierwszego komunikatu! \n\n\nAlert zostal wygenerowany automatycznie - nie odpowiadaj na niego.' | /usr/bin/mailx -s 'Sprawdz poziom pelletu' ala24.as@gmail.com -a 'From: Kotlownia Sliwice'")
-      os.system("/bin/echo -e 'Sprawdz poziom pelletu w zasobniku. \nZasypu wystarczy tylko na ok 1.5 dnia palenia, liczac od pierwszego komunikatu! \n\n\nAlert zostal wygenerowany automatycznie - nie odpowiadaj na niego.' | /usr/bin/mailx -s 'Sprawdz poziom pelletu' ksawery@spoko.pl -a 'From: Kotlownia Sliwice'") 
+      os.system("/bin/echo -e 'Sprawdz poziom pelletu w zasobniku. \nZasypu wystarczy tylko na ok 1.5 dnia palenia, liczac od pierwszego komunikatu! \n\n\nAlert zostal wygenerowany automatycznie - nie odpowiadaj na niego.' | /usr/bin/mailx -s 'Sprawdz poziom pelletu' examp1@eample.com -a 'From: Kotlownia *****'")
+      os.system("/bin/echo -e 'Sprawdz poziom pelletu w zasobniku. \nZasypu wystarczy tylko na ok 1.5 dnia palenia, liczac od pierwszego komunikatu! \n\n\nAlert zostal wygenerowany automatycznie - nie odpowiadaj na niego.' | /usr/bin/mailx -s 'Sprawdz poziom pelletu' examp2@eample.com -a 'From: Kotlownia *****'")
+      os.system("/bin/echo -e 'Sprawdz poziom pelletu w zasobniku. \nZasypu wystarczy tylko na ok 1.5 dnia palenia, liczac od pierwszego komunikatu! \n\n\nAlert zostal wygenerowany automatycznie - nie odpowiadaj na niego.' | /usr/bin/mailx -s 'Sprawdz poziom pelletu' examp3@eample.com -a 'From: Kotlownia *****'") 
   sleep(14400)
 
 
