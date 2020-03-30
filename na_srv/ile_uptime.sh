675633be8d0cad7b5849ed3289117308fc795f1f
Roman Sokalski <62810569+romsok24@users.noreply.github.com>
2020-03-30 19:18:45 +0200

    Update ile_uptime.sh

--- na_srv/ile_uptime.sh
+++ na_srv/ile_uptime.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-ile=$(ssh  admin@10.200.10.18 uptime | awk '{print $3}' | sed 's/,//' |  sed 's/://')
-jednostka=$(ssh  admin@10.200.10.18 uptime | awk '{print $4'} | sed 's/,//')
+ile=$(ssh  admin@10.***.***.*** uptime | awk '{print $3}' | sed 's/,//' |  sed 's/://')
+jednostka=$(ssh  admin@10.***.***.*** uptime | awk '{print $4'} | sed 's/,//')
 echo "Router pracuje od $ile$jednostka"
 if (( $ile < 4 )); then 
   if [ "$jednostka" = "min" ]; then
     if [ ! -f /tmp/byl_restart_asusa ]; then
       touch /tmp/byl_restart_asusa
       echo "Wykryto restart routera $ile min. temu"
-      echo "Subject: Wykryto restart routera $ile min. temu" | /usr/sbin/ssmtp 24roman12kam1@gmail.com
+      echo "Subject: Wykryto restart routera $ile min. temu" | /usr/sbin/ssmtp examp1@eample.com
       sleep 240 &&
-      ssh  admin@10.200.10.18 ip r add default via  10.200.10.17
+      ssh  admin@10.***.***.*** ip r add default via  10.***.***.***
       rm -f /tmp/byl_restart_asusa
     fi
   fi
