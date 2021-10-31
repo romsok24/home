c3cec0460eac36052b535b509395dea45fcc37b0
Roman Sokalski <62810569+romsok24@users.noreply.github.com>
2021-10-31 13:09:11 +0100

    Update pellet.py

--- pellet.py
+++ pellet.py
@@ -1,3 +1,4 @@
+# - *- coding: utf- 8 - *-
 # Skrypt monitorujacy poziom pelletu w zasobniku
 # Realizacja na podst. monitorowania poziomu na wejsciu GPIO21 rpizero01
 # Konfiguracja maili: https://wiki.archlinux.org/title/Msmtp#Server_sent_empty_reply
@@ -5,16 +6,21 @@
 import RPi.GPIO as GPIO, os
 from time import *
 from datetime import date, datetime
+import os
+
+def include(filename):
+    if os.path.exists(filename):
+        execfile(filename)
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(21, GPIO.IN)
+print('Czekam na rozpoczęcie....')
 sleep(60)
 
 while True:
   dateczka=datetime.now().strftime("%d %b, %H:%M")
   if(GPIO.input(21) == 0):
-      print('Uwaga - sprawdz poziom pelletu w zasobniku! Stan na dzien {dateczka}'.format(dateczka=dateczka))
-      os.system("printf 'Subject: Sprawdź poziom pelletu\nFrom: Kolownia Sliwice\nSprawdź poziom pelletu w zasobniku. \nZasypu wystarczy tylko na ok 1.5 dnia palenia, liczac od pierwszego komunikatu! \n\n\nAlert został wygenerowany automatycznie - nie odpowiadaj na niego.' | /usr/bin/msmtp  examp1@eample.com")
+        include('/home/pi/dane/mail.py')
+  print('Czekam 4h do następnego sprawdzenia...')
   sleep(14400)
-
-
+  
